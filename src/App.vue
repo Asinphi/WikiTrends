f3beee48bfdf133ea9574a68e603a6d62baff3d1
Lawson <abbylaw247@gmail.com>
2024-03-15 02:58:20 -0400

    Added logo/temp main page
    (Just testing out Vue)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Temp logo" src="./assets/logo.png">
+  <HelloWorld msg="Temporary Home"/>
 </template>
 
 <script>
