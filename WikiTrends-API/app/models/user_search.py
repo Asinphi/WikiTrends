569ba67976d6b9291dd4c01dfe5e1ab8a8353c61
sensen312 <sensen312school@gmail.com>
2024-04-04 11:26:25 -0400

    oops forgot a model

---
+++ WikiTrends-API/app/models/user_search.py
@@ -0,0 +1,5 @@
+class UserSearch:
+    def __init__(self, search_id, search_date, search_term):
+        self.search_id = search_id
+        self.search_date = search_date
+        self.search_term = search_term
