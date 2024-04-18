5ca1701c0d9c9d49ce37c31eef28e8d63bc8345a
sensen312 <sensen312school@gmail.com>
2024-04-18 00:36:31 -0400

    BRUHH

--- WikiTrends-API/app/models/user_search.py
+++ WikiTrends-API/app/models/user_search.py
@@ -1,5 +1,6 @@
 class UserSearch:
-    def __init__(self, search_id, search_date, search_term):
+    def __init__(self, search_id=None, search_date=None, search_term=None, total_views=None):
         self.search_id = search_id
         self.search_date = search_date
         self.search_term = search_term
+        self.total_views = total_views
--- WikiTrends-API/app/services/user_search_service.py
+++ WikiTrends-API/app/services/user_search_service.py
@@ -1,4 +1,6 @@
 from app.repositories.user_search_repository import UserSearchRepository
+from app.models.user_search import UserSearch
+from datetime import datetime
 
 class UserSearchService:
     def __init__(self, db_config):
