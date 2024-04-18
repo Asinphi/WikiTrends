d5cf2676a40f12205218a187e493fc860bfe1771
terrynelson-tn <tnelson11720@gmail.com>
2024-04-17 20:49:29 -0400

    cleaned up code

--- test_project/website_db_connection.py
+++ test_project/website_db_connection.py
@@ -11,51 +11,7 @@ def print_dict(x):
 
 app = Flask(__name__)
 cors = CORS(app)
-# con = cx_oracle.connect(user="terry.nelson", password="6G8IWcIQQsswp0Rw7hN1X6qO", dsn="")
-
-try:
-  app.config['SQLALCHEMY_DATABASE_URI'] = "oracle+cx_oracle://terry.nelson:6G8IWcIQQsswp0Rw7hN1X6qO@oracle.cise.ufl.edu:1521/orcl"
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  db = SQLAlchemy(app)
-  # db.init_app(app)
-except Exception as e:
-  print("Error: ", e)
-
 connection_string = "oracle+cx_oracle://terry.nelson:6G8IWcIQQsswp0Rw7hN1X6qO@oracle.cise.ufl.edu:1521/orcl"
-# primary_key=True in every column when a table has no primary key: 
-# https://docs.sqlalchemy.org/en/13/faq/ormconfiguration.html#how-do-i-map-a-table-that-has-no-primary-key:~:text=Better%20yet%20is%20when%20using%20fully%20declared%20table%20metadata%2C%20use%20the%20primary_key%3DTrue%20flag%20on%20those%20columns%3A
-class Article(db.Model): 
-  project = db.Column(db.String(33), primary_key=True)
-  article = db.Column(db.String(33), primary_key=True)
-  granularity = db.Column(db.String(33), primary_key=True)
-  timestamp = db.Column(db.Integer, primary_key=True)
-  access = db.Column(db.String(33), primary_key=True)
-  agent = db.Column(db.String(33), primary_key=True)
-  views = db.Column(db.Integer, primary_key=True)
-
-engine = create_engine(connection_string)
-try:
-  connection = engine.connect()
-except Exception as e:
-  print("\nError: ", e)
-  print("Could be bad username/password")
-
-# enter any table name
-table_name = "einstein"
-with connection:
-  try:
-    if connection.dialect.has_table(connection,table_name=table_name):
-      print("table exists")
-      query = connection.execute(text(f"select * from {table_name}"))
-      # q = query.__dict___()
-      # for i in q:
-      #   print(i) 
-      query_as_dict = query.mappings().all()
-      print(pprint.pformat(query_as_dict))
-  except Exception as e:
-    print("Something went wrong: ", e)
-
-# class Article:
 
 @app.route('/api/search')
 def search():
