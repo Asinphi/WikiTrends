71a457a62824cd335c7b02dd3878c3a17d7bda11
sensen312 <sensen312school@gmail.com>
2024-04-17 22:24:56 -0400

    Revert "CORS"
    This reverts commit 42b188400fe9006c8de35790a9bb7a362a67332b.

--- WikiTrends-API/app.py
+++ WikiTrends-API/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 from app.controllers.article_controller import article_controller
 from app.controllers.page_view_controller import page_view_controller
 from app.controllers.user_search_controller import user_search_controller
@@ -7,7 +6,6 @@ from app.controllers.category_controller import category_controller
 from config import db_config
 
 app = Flask(__name__)
-CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_config.url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
